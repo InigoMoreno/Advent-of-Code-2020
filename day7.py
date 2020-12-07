2286bdddaed41d22ee753f27d73a7e01b2606c5a
Iñigo Moreno <ignigomoreno@gmail.com>
2020-12-07 09:30:55 +0000

    Removed old code

--- day7.py
+++ day7.py
@@ -22,18 +22,6 @@ def find_containers(treasure):
         if treasure in items:
             find_containers(bag)
 
-found_containers_old=dict()
-def find_containers_old(treasure):
-    if treasure in found_containers_old: 
-        return found_containers_old[treasure]
-    s = set()
-    for bag, items in containing.items():
-        if treasure in items:
-            s.add(bag)
-            s=s.union(find_containers_old(bag))
-    found_containers_old[treasure]=s
-    return s
-
 def part2():
     return count_items('shiny gold')-1
 
